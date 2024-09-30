c09427ac88246f14a54ed63ceb94c86132429bb6
RaduTuodr <tudoreduard2004@gmail.com>
2024-09-30 13:18:49 +0300

    small fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express'
 const app = express()
 
 // get the port from env variable
-const PORT = process.env.port || 8080
+const PORT = 8080
 
 app.use(express.static('dist'))
 
